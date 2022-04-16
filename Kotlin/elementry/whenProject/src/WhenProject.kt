320bbed8dd70ab2ec9994b2e0f7f464f180497e9
Abtinz <abtinzandi@gmail.com>
2022-04-16 10:07:19 +0430

    when with return

--- Kotlin/elementry/whenProject/src/WhenProject.kt
+++ Kotlin/elementry/whenProject/src/WhenProject.kt
@@ -1,7 +1,7 @@
 import java.util.*
 
 fun main() {
-   println("PLEASE ENTER YOUR SPECIAL DAY OF WEEK ")
+   println("PLEASE ENTER YOUR SPECIAL DAY OF WEEK\nguid : Week indexes are starting of 0(saturday) to 6(friday)")
    var scanner = Scanner(System.`in`)
    var dateIndex = scanner.nextInt()
    when (dateIndex){
@@ -15,6 +15,21 @@ fun main() {
        else -> println("LETS GO TO THE NEXT WEEK")
    }
 
+    if(dateIndex>6){
+        var reminderOfDateIndex = dateIndex % 7
+        var dateRes = ""
+        dateRes = when (reminderOfDateIndex){
+            0 -> "saturday"
+            1 -> "sunday"
+            2 -> "monday"
+            3 -> "tuesday"
+            4 -> "wednesday"
+            5 -> "Thursday"
+            6 -> "Friday"
+            else-> "This Branch wont be used! "
+        }
+        println("correct date of week: ${dateRes}")
+    }
 
 
 }
