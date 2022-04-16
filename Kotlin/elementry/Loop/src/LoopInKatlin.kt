6095d18a59cc9dfe26c24a855f97e8c9f05ccd1f
Abtinz <abtinzandi@gmail.com>
2022-04-16 13:46:28 +0430

    LOOP step and until

---
+++ Kotlin/elementry/Loop/src/LoopInKatlin.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Odd Numbers in (0,1000):\n//step by step")
+    //show them with steps
+    for(index in 1 until 1000 step 2){
+        println(index)
+    }
+
+    println("Odd Numbers in (0,1000):\n//conditions")
+    //show them with conditions
+    for(index in 1 until 1000){
+        if(index % 2 != 0)
+                println(index)
+    }
+}
