10b891db8e96dc075b4a1ffb09edfa9094ad4e50
Abtinz <abtinzandi@gmail.com>
2022-05-20 22:31:57 +0430

    lets make xml layout (simple with fxml and textView ... )

--- Android Studio/ConvertAgeToMinutepRroject/app/src/main/java/com/abtinAndroidStdio/convertagetominute/MainActivity.kt
+++ Android Studio/ConvertAgeToMinutepRroject/app/src/main/java/com/abtinAndroidStdio/convertagetominute/MainActivity.kt
@@ -2,10 +2,22 @@ package com.abtinAndroidStdio.convertagetominute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)    //set page of app with r.layout
+
+        //calender button (handle button and Calender
+        var calenderButton = findViewById<Button>(R.id.chooseDateFromCalanderButton)
+        //in this section we are setting a listener for our button
+        calenderButton.setOnClickListener {
+            
+        }
+
+
     }
+
+
 }
