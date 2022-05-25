559f2705cf618eaf4c74e650158cf8f80f962674
Abtinz <abtinzandi@gmail.com>
2022-05-25 22:37:45 +0430

    new feature for app (number text view and calculate Button)

--- Android Studio/ConvertAgeToMinutepRroject/app/src/main/java/com/abtinAndroidStdio/convertagetominute/MainActivity.kt
+++ Android Studio/ConvertAgeToMinutepRroject/app/src/main/java/com/abtinAndroidStdio/convertagetominute/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
@@ -15,10 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)    //set page of app with r.layout
 
         //calender button (handle button and Calender
-        var calenderButton = findViewById<Button>(R.id.chooseDateFromCalanderButton)
+        val calenderButton = findViewById<Button>(R.id.chooseDateFromCalanderButton)
+        val calcualteButton = findViewById<Button>(R.id.calculateButton)
         //text views off birthDay date and Result
         val userBirthDayDate = findViewById<TextView>(R.id.enterdBirthDayDate)
         val ageToMinute = findViewById<TextView>(R.id.AgeToMinute)
+        //number text views
+        var yearNumber = findViewById<TextView>(R.id.YearNumber)
+        var monthNumber = findViewById<TextView>(R.id.MonthDate)
+        var dayNumber = findViewById<TextView>(R.id.dayDate)
         //in this section we are setting a listener for our button
         calenderButton.setOnClickListener {
             //calender and current time in Kt and Android Std
@@ -32,7 +36,7 @@ class MainActivity : AppCompatActivity() {
                     //the user information
                     view, year, month, dayOfMonth ->
 
-                   var userEnterdDate = "${dayOfMonth}--${month}--${year}"
+                   var userEnterdDate = "${dayOfMonth}--${month+1}--${year}"
                    //date formatter and its pattern
                    val dateFormatter = SimpleDateFormat("dd--mm--yy")
                    var userSelectedDate = dateFormatter.parse(userEnterdDate)
@@ -49,6 +53,27 @@ class MainActivity : AppCompatActivity() {
                     currentYear , currentMonth , currentDay)
             datePicker.datePicker.maxDate = Date().time
             datePicker.show()
+        }
+
+        calcualteButton.setOnClickListener {
+
+            
+            var userEnterdDate = "${dayNumber.text}--${monthNumber.text}--${yearNumber.text}"
+
+            val dateFormatter = SimpleDateFormat("dd--mm--yy")
+            var userSelectedDate = dateFormatter.parse(userEnterdDate)
+            val minuteOfBirthDayDate = userSelectedDate.time / 6000
+
+            var systemMinute = System.currentTimeMillis()
+            var currentDay = dateFormatter.parse(dateFormatter.format(systemMinute))
+            var currentMinute = currentDay.time / 6000
+
+            var timeDifference = currentMinute - minuteOfBirthDayDate
+            userBirthDayDate.setText(userEnterdDate)
+            ageToMinute.setText(timeDifference.toString())
+
+
+
         }
 
 
