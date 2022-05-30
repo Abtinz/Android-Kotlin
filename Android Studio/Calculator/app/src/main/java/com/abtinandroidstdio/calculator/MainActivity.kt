b9ffb96bc071ba77dbe1a2a12f66bc613114fc81
Abtinz <abtinzandi@gmail.com>
2022-05-30 22:47:06 +0430

    ActivityMainBinding and binding val

--- Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
+++ Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.abtinandroidstdio.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.abtinandroidstdio.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding view for easily id access
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(R.layout.activity_main)
     }
 }
