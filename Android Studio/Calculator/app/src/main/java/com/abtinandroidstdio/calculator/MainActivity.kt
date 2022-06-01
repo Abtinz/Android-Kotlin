6db29857992bdd090f48eb11dbff70b1c6a0beba
Abtinz <abtinzandi@gmail.com>
2022-06-01 23:57:23 +0430

    onclickdigit fun and onclick for views in xml then we cast the view to button and append the new clicked number to our calculator view

--- Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
+++ Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import com.abtinandroidstdio.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         // binding. root will help us to alter our views with bulding and its like R.layout.activity_main
     }
 
-    fun onclickDigit(view : View){
-        
+    /*
+        in this method we are casting view to button and then we will append the text of button to textView
+        this is how we add the new clicked number to calculator view
+     */
+    fun onclickDigit(clickedNumber : View){
+        val clickedButton = clickedNumber as Button
+        binding.textView.append(clickedButton.text)
     }
 }
