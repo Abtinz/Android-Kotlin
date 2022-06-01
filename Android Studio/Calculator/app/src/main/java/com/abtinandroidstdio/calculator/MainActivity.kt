9ab0a6d42dc02a78d84a516cab9e270ed245fdd2
Abtinz <abtinzandi@gmail.com>
2022-06-02 00:18:48 +0430

    dot click fun adnd clearing button implication

--- Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
+++ Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
@@ -9,20 +9,48 @@ import com.abtinandroidstdio.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding //because of scope var
+    var isSomeDigitEntered = false
+    var isDotButtonClicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //binding view for easily id access
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this)) // alt enter --> split properties
         setContentView(binding.root)
-        // binding. root will help us to alter our views with bulding and its like R.layout.activity_main
+        // binding. root will help us to alter our views with building and its like R.layout.activity_main
     }
 
     /*
-        in this method we are casting view to button and then we will append the text of button to textView
+        in this function we are casting view to button and then we will append the text of button to textView
         this is how we add the new clicked number to calculator view
      */
     fun onclickDigit(clickedNumber : View){
         val clickedButton = clickedNumber as Button
-        binding.textView.append(clickedButton.text)
+        if (isSomeDigitEntered){
+            binding.textView.append(clickedButton.text)
+        }
+        else {
+            binding.textView.text = clickedButton.text
+            isSomeDigitEntered = true
+        }
+    }
+
+    /*
+         in this function we are checking entered dot or new dot symbol for floating point number
+     */
+    fun onDotClicked(view : View) {
+        if (!isDotButtonClicked && isSomeDigitEntered){
+            binding.textView.append(".")
+            isDotButtonClicked = true
+        }
+    }
+
+    /*
+        in this fun we are clearing all numbers of calculator view
+     */
+    fun clearAllNumbers(view: View){
+        binding.textView.text = ""
+        isSomeDigitEntered = false
+        isDotButtonClicked = false
     }
 }
