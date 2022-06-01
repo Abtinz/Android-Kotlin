18291ea4a919704d58c2bc427cfabb68b0d34742
Abtinz <abtinzandi@gmail.com>
2022-06-01 23:48:13 +0430

    binding root and binding scpoe enter and alt --> split properties binding root == R.Layout.***

--- Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
+++ Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
@@ -3,13 +3,20 @@ package com.abtinandroidstdio.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import com.abtinandroidstdio.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding //because of scope var
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //binding view for easily id access
-        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this)) // alt enter --> split properties
+        setContentView(binding.root)
+        // binding. root will help us to alter our views with bulding and its like R.layout.activity_main
+    }
+
+    fun onclickDigit(view : View){
+        
     }
 }
