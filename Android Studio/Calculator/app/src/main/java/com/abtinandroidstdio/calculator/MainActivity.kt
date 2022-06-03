13fd36d8e8a3c688051a14a51984b0a4f465e321
Abtinz <abtinzandi@gmail.com>
2022-06-03 23:50:16 +0430

    Toast and operation adding

--- Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
+++ Android Studio/Calculator/app/src/main/java/com/abtinandroidstdio/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.abtinandroidstdio.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -45,6 +46,15 @@ class MainActivity : AppCompatActivity() {
             binding.textView.append(".")
             isDotButtonClicked = true
         }
+
+        else if(!isSomeDigitEntered){
+            Toast.makeText(this,"You have not entered a number yet" , Toast.LENGTH_LONG).show()
+        }
+
+        else{
+            Toast.makeText(this,"You have entered dot symbol" , Toast.LENGTH_LONG).show()
+
+        }
     }
 
     /*
@@ -54,24 +64,29 @@ class MainActivity : AppCompatActivity() {
         binding.textView.text = ""
         isSomeDigitEntered = false
         isDotButtonClicked = false
+        isOperatoinEntered = false
+        isSecondNumberEntered = false
     }
 
     /*
-        in this function we are casting view to button and then we will append the text of button to textView
-        this is how we add the new clicked number to calculator view
+        in this function we are adding operation to our calculator text view
      */
     fun onclickOperator(clickedNumber : View){
         val clickedButton = clickedNumber as Button
         if (!isSomeDigitEntered){
-
+            Toast.makeText(this,"You have not entered a number yet" , Toast.LENGTH_LONG).show()
         }
         else {
             if(isOperatoinEntered){
                 var textViewString = binding.textView.text
                 //how to use substring in kotlin
-                textViewString = textViewString.substring(0, textViewString.length - 1) + clickedButton.text
+                binding.textView.text = textViewString.substring(0, textViewString.length - 1) + clickedButton.text
             }
-            binding.textView.append(clickedButton.text)
+            else{
+                binding.textView.append(clickedButton.text)
+                isOperatoinEntered = true
+            }
+
         }
     }
 
