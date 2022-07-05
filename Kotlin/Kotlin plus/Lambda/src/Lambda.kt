7d58278b9191abf1559542d650dbe81bbaf09521
Abtinz <abtinzandi@gmail.com>
2022-07-06 00:27:29 +0430

    how to pass a lambda function like parameter

--- Kotlin/Kotlin plus/Lambda/src/Lambda.kt
+++ Kotlin/Kotlin plus/Lambda/src/Lambda.kt
@@ -1,7 +1,16 @@
+import java.util.Scanner
+
 fun main(){
     //cast lambda
-    var c = {stringKey : String  ->
+    val lambda_function = {stringKey : String  ->
         println(stringKey.toInt())
     }
+    getInput(lambda_function)
 
+}
+fun getInput(lambda_function: (String) -> Unit){
+    print("Please enter your string to cast: ")
+    val scanner = Scanner(System.`in`)
+    var casting_string = scanner.next()
+    lambda_function(casting_string)
 }
