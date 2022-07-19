0e72f4512e1df91f3563966d232ad19efeaede3c
Abtinz <abtinzandi@gmail.com>
2022-07-20 01:59:11 +0430

    simple binding of second page and user name

--- Android Studio/QuizApp/app/src/main/java/com/abtinandroidstdio/quizapp/QuestionActivity.kt
+++ Android Studio/QuizApp/app/src/main/java/com/abtinandroidstdio/quizapp/QuestionActivity.kt
@@ -2,13 +2,21 @@ package com.abtinandroidstdio.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.abtinandroidstdio.quizapp.databinding.ActivityQuestionBinding
 import com.abtinandroidstdio.quizapp.databinding.ActivityMainBinding
 class QuestionActivity : AppCompatActivity() {
-    lateinit var Question_binding : ActivityQuestionBinding
-    lateinit var main_binding : ActivityMainBinding
+    lateinit var questionBinding : ActivityQuestionBinding
+    lateinit var mainBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        questionBinding = ActivityQuestionBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_question)
+        questionBinding.UsernameText.text = "UserName: " + mainBinding.UserNameTextView.text
+
+        setContentView(questionBinding.root)
+
     }
+
+
 }
