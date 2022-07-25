bceaec262ef7e3f5c57fa50bd497d792a79bce74
Abtinz <abtinzandi@gmail.com>
2022-07-25 12:51:47 +0430

    lets make a basic xml for option background

--- Android Studio/QuizApp/app/src/main/java/com/abtinandroidstdio/quizapp/Question.kt
+++ Android Studio/QuizApp/app/src/main/java/com/abtinandroidstdio/quizapp/Question.kt
@@ -2,11 +2,13 @@ package com.abtinandroidstdio.quizapp
 
 data class Question(
     val id : Int,
+    val questionText : String,
+    val Image : Int ,
     val firstQuestionText : String,
     val secondQuestionText : String,
     val thirdQuestionText : String,
     val fourthQuestionText : String,
-    val correctAnswerId : Int,
-    val questionText : String
+    val correctAnswerId : Int
+
 )
 
