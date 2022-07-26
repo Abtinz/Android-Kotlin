35cd2227a77128a7a126211f48a9ef6aefc15dea
Abtinz <abtinzandi@gmail.com>
2022-07-26 21:44:53 +0430

    def questionCreator and

--- Android Studio/QuizApp/app/src/main/java/com/abtinandroidstdio/quizapp/QuestionActivity.kt
+++ Android Studio/QuizApp/app/src/main/java/com/abtinandroidstdio/quizapp/QuestionActivity.kt
@@ -2,19 +2,23 @@ package com.abtinandroidstdio.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TextureView
+import android.view.View
 import com.abtinandroidstdio.quizapp.databinding.ActivityQuestionBinding
 
 class QuestionActivity : AppCompatActivity() {
     lateinit var questionBinding : ActivityQuestionBinding
+    lateinit var questionsList : ArrayList<Question>
+    var currentQuestionId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         questionBinding = ActivityQuestionBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
         setContentView(questionBinding.root)
+        questionsList = Constants.getQuestions()
+        questionCreator()
+    }
 
-        val questionsList = Constants.getQuestions()
-
-        val currentQuestionId = 0
+    fun questionCreator(){
         val currentQuestion = questionsList[currentQuestionId]
         questionBinding.questionText.text = currentQuestion.questionText
         questionBinding.firstOption.text = currentQuestion.firstOptionText
@@ -23,9 +27,17 @@ class QuestionActivity : AppCompatActivity() {
         questionBinding.fourthOption.text = currentQuestion.fourthOptionText
         questionBinding.questionImage.setImageResource(currentQuestion.Image)
         questionBinding.prograssTextView.text = "[$currentQuestionId / ${questionBinding.progressBar.max}]"
+    }
 
+    fun oneOptionClicked(view : View){
+        view as TextureView
+    }
+
+    fun resetOptions(){
 
     }
+
+
 
 
 }
