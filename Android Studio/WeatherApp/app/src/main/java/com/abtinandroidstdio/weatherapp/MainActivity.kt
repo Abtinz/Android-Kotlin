e600a8546740a386df5ec40b61be5e0bc8362881
Abtinz <abtinzandi@gmail.com>
2022-07-31 23:58:30 +0430

    build client and request with okhttp

--- Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/MainActivity.kt
+++ Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.abtinandroidstdio.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var client = OkHttpClient()
+        var rquest = Request.Builder()
     }
 }
