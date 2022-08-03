e1c2cd89f09a86c908339748b31a2a684446fe7b
Abtinz <abtinzandi@gmail.com>
2022-08-04 00:27:33 +0430

    debug and toast !

--- Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/MainActivity.kt
+++ Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.abtinandroidstdio.weatherapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,22 +8,37 @@ import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    lateinit var  jsonObject :JSONObject
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialFunc()
+    }
 
+    fun initialFunc(){
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=a5ab76fae5a9227d96ff3adf0b793c8f&lang=fa")
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                val rawJSONContex = response.body!!.string()
+                Log.d("tagJson" , "Json: ${rawJSONContex}")
+                val jsonObject = JSONObject(rawJSONContex)
+                
+                runOnUiThread {
+                    ToatMaker(jsonObject)
+                }
 
-        val apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=a5ab76fae5a9227d96ff3adf0b793c8f&lang=fa"
-        val jsonDownloaderObject = JsonDownloader(apiUrl)
-        val jsonStr = jsonDownloaderObject.downloadFunction()
-        if(jsonStr.equals("Fail")){
-            Toast
-        }
-        jsonObject = JSONObject()
+            }
 
+        })
+    }
 
-
-
+    fun ToatMaker(jsonObject:JSONObject){
+        val cityName = jsonObject.getString("name")
+        Toast.makeText(this,cityName,Toast.LENGTH_SHORT).show()
     }
 }
