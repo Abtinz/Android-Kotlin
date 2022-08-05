d0f00071d7696b6870591dafac4b5f0d7da49aa2
Abtinz <abtinzandi@gmail.com>
2022-08-05 14:35:10 +0430

    Weather app xml design

--- Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/MainActivity.kt
+++ Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/MainActivity.kt
@@ -3,11 +3,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.abtinandroidstdio.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    var appLanguage = "fa"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,7 +25,17 @@ class MainActivity : AppCompatActivity() {
         }else{
             val intent = Intent(this , WeatherActivity::class.java)
             intent.putExtra("cityName" , cityName)
+            intent.putExtra("appLanguage",appLanguage)
             startActivity(intent)
+        }
+    }
+
+    fun setLanguage(view : View){
+        val button = view as Button
+        if(button.text.equals("English")){
+            appLanguage = "en"
+        }else{
+            appLanguage = "fa"
         }
     }
 
