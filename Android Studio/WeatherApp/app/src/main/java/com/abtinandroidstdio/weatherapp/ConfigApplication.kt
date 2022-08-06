f8734683a0d0aa5ef3c82fa516b9b5bfd1b8fc02
Abtinz <abtinzandi@gmail.com>
2022-08-06 23:04:27 +0430

    cofigclass will inheritant from Aplication to creat a font with Calligraphy

---
+++ Android Studio/WeatherApp/app/src/main/java/com/abtinandroidstdio/weatherapp/ConfigApplication.kt
@@ -0,0 +1,25 @@
+package com.abtinandroidstdio.weatherapp
+
+
+import android.app.Application
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
+
+class ConfigApplication(): Application(){
+    override fun onCreate() {
+        super.onCreate()
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("font/vazimedium.ttf")
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
+}
