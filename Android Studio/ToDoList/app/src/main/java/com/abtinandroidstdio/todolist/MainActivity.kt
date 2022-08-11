a82dcf42d38ddccbe9b35db52e2acd5193332285
Abtinz <abtinzandi@gmail.com>
2022-08-11 19:23:49 +0430

    JSON AND API AND TODO OBJECT

--- Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/MainActivity.kt
+++ Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.abtinandroidstdio.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.abtinandroidstdio.todolist.databinding.ActivityMainBinding
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        jsonDownloader()
     }
+
+     fun  jsonDownloader(){
+        val client = OkHttpClient()
+        val url = "https://jsonplaceholder.typicode.com/todos"
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                jsonDownloader()
+            }
+            override fun onResponse(call: Call, response: Response) {
+                val rawJSONContext = response.body!!.string()
+                Log.d("tagJson" , "Json: ${rawJSONContext}")
+                val jsonObject = JSONObject(rawJSONContext)
+
+                runOnUiThread {
+
+                    intentFunc(jsonObject)
+
+                }
+
+            }
+
+        })
+    }
+
+    fun JsonToDoList(jsonObject:JSONObject){
+
+    }
+    fun intentFunc(jsonObject:JSONObject){
+        val intent = Intent(this , ToDoListActivity::class.java)
+        //intent.putExtra("jsonObject" , jsonObject)
+
+        startActivity(intent)
+    }
+
 }
