91d17248889ce585d8a8ee57a869d1f2813bf104
Abtinz <abtinzandi@gmail.com>
2022-08-12 21:14:53 +0430

    ActivityInputBinding is created

--- Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/InputActivity.kt
+++ Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/InputActivity.kt
@@ -2,10 +2,12 @@ package com.abtinandroidstdio.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.abtinandroidstdio.todolist.databinding.ActivityInputBinding
 class InputActivity : AppCompatActivity() {
+    lateinit var binding : ActivityInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input)
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
