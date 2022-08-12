5bcbd09b2f051e93ea85d835d0af1358b5422117
Abtinz <abtinzandi@gmail.com>
2022-08-12 21:48:51 +0430

    searchUserId implemention

--- Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/InputActivity.kt
+++ Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/InputActivity.kt
@@ -1,5 +1,6 @@
 package com.abtinandroidstdio.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -17,16 +18,41 @@ class InputActivity : AppCompatActivity() {
     }
 
 
-    fun searchId(userIdList:ArrayList<Int>,taskIdList:ArrayList<Int>,taskTextList:ArrayList<String>,taskStateList:ArrayList<Boolean>){
+    fun searchUserId(userIdList:ArrayList<Int>,taskIdList:ArrayList<Int>,taskTextList:ArrayList<String>,taskStateList:ArrayList<Boolean>,userId:Int){
         var isCorrect = false
-        if(isCorrect){
 
+        var userTaskIdList = ArrayList<Int>()
+        var userTaskTextList = ArrayList<String>()
+        var userTaskStateList = ArrayList<Boolean>()
+
+        var index = 0
+        while(index<userIdList.size){
+
+            if(userIdList[index] == userId){
+                isCorrect = true
+                userTaskIdList.add(taskIdList[index])
+                userTaskTextList.add(taskTextList[index])
+                userTaskStateList.add(taskStateList[index])
+            }
+
+
+            index -=-1
+        }
+        if(isCorrect){
+            val intentOfInputActivity = Intent(this , ToDoListActivity::class.java)
+
+            intentOfInputActivity.putExtra("inputType","userId")
+            intentOfInputActivity.putExtra("userId",userId)
+            intentOfInputActivity.putExtra("taskIdList",userTaskIdList)
+            intentOfInputActivity.putExtra("taskTextList",userTaskTextList)
+            intentOfInputActivity.putExtra("taskStateList",userTaskStateList)
+            startActivity(intentOfInputActivity)
         }else{
-            Toast.makeText(this,"Is Invalid",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"User Id Is Invalid!",Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun searchUserId(userIdList:ArrayList<Int>,taskIdList:ArrayList<Int>,taskTextList:ArrayList<String>,taskStateList:ArrayList<Boolean>){
+    fun searchId(userIdList:ArrayList<Int>,taskIdList:ArrayList<Int>,taskTextList:ArrayList<String>,taskStateList:ArrayList<Boolean>){
 
         var isCorrect = false
         if(isCorrect){
