4c942a285c69966f006e4faace447b112b3f714d
Abtinz <abtinzandi@gmail.com>
2022-08-12 20:26:11 +0430

    input activity added

---
+++ Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/InputActivity.kt
@@ -0,0 +1,11 @@
+package com.abtinandroidstdio.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
