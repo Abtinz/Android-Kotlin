44759a14e4c40f55e8074502121f894796885140
Abtinz <abtinzandi@gmail.com>
2022-08-14 17:38:45 +0430

    Lets Make grid adopter

--- Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/ToDoListActivity.kt
+++ Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/ToDoListActivity.kt
@@ -1,7 +1,11 @@
 package com.abtinandroidstdio.todolist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import com.abtinandroidstdio.todolist.databinding.ActivityToDoListBinding
 class ToDoListActivity : AppCompatActivity() {
     lateinit var binding :ActivityToDoListBinding
@@ -37,8 +41,28 @@ class ToDoListActivity : AppCompatActivity() {
 
     }
 
+    class CustomAdapter( var context: Context,
+                         var itemModel:ArrayList<Todo>):BaseAdapter(){
+          override fun getCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+          override fun getItem(p0: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemId(p0: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            TODO("Not yet implemented")
+        }
+
+    }
+
     fun loadTaskByTitle(){
-        
+
         val userId = intent.getIntExtra("userId",1)
         val taskId = intent.getIntExtra("taskId",1)
         val taskText = intent.getStringExtra("taskText").toString()
@@ -69,4 +93,7 @@ class ToDoListActivity : AppCompatActivity() {
             todoList.add(todo)
         }
     }
+
+
+
 }
