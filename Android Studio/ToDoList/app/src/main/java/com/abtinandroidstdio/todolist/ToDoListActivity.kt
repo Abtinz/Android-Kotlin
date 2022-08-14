1b44f63fe977a3458e4eeae28eadca361600e672
Abtinz <abtinzandi@gmail.com>
2022-08-14 15:43:46 +0430

    task loading structure

--- Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/ToDoListActivity.kt
+++ Android Studio/ToDoList/app/src/main/java/com/abtinandroidstdio/todolist/ToDoListActivity.kt
@@ -2,10 +2,36 @@ package com.abtinandroidstdio.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.abtinandroidstdio.todolist.databinding.ActivityToDoListBinding
 class ToDoListActivity : AppCompatActivity() {
+    lateinit var binding :ActivityToDoListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_to_do_list)
+        binding =  ActivityToDoListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadTask()
+    }
+
+    private fun loadTask(){
+        when(intent.getStringExtra("inputType").toString()){
+            "UserId" ->  loadTaskByUserId()
+            "TaskId" ->  loadTaskByTaskId()
+            "Title" ->  loadTaskByTitle()
+            "Top50" ->  loadTaskByTop50()
+        }
+    }
+
+    fun loadTaskByUserId(){
+
+    }
+
+    fun loadTaskByTitle(){
+
+    }
+    fun loadTaskByTaskId(){
+
+    }
+    fun loadTaskByTop50(){
+
     }
 }
