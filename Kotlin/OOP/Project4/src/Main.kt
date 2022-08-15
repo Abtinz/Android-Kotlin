b005655f6a30267ab4763c2a11a14a5e7847696d
Abtinz <abtinzandi@gmail.com>
2022-08-15 23:38:30 +0430

    ops i forgot to add casting  project!

--- Kotlin/OOP/Project4/src/Main.kt
+++ Kotlin/OOP/Project4/src/Main.kt
@@ -1,8 +1,13 @@
 fun main() {
 
-  var course = Course(2000 , "Finished" , "Android Studio" ,
+  var teacher = Teacher("Amir AHmad Adibie","99@%339","Android Studio","MaktabKhoneh","MaktabKhoneh.org Web School")
+  val course = Course(2000 , "Finished" , "Android Studio" ,
     "Amir AHmad Adibie","99@%339","MaktabKhoneh","MaktabKhoneh.org Web School")
   course.printDetails("Teacher")
+
+  val catedCourse = course as Teacher
+  println("Casted Teacher:")
+  catedCourse.printTeacherDetails()
 
   var student = Student("Arash Moradi" , "200#489" , "19.5" ,
                         "202.B","MaktabKhoneh","MaktabKhoneh.org Web School")
