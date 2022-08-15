0819627bd56644e8997f4ad4c8d0ba7930864e41
Abtinz <abtinzandi@gmail.com>
2022-08-15 23:53:33 +0430

    cast with condition

--- Kotlin/OOP/Project4/src/Main.kt
+++ Kotlin/OOP/Project4/src/Main.kt
@@ -1,6 +1,5 @@
 fun main() {
 
-  var teacher = Teacher("Amir AHmad Adibie","99@%339","Android Studio","MaktabKhoneh","MaktabKhoneh.org Web School")
   val course = Course(2000 , "Finished" , "Android Studio" ,
     "Amir AHmad Adibie","99@%339","MaktabKhoneh","MaktabKhoneh.org Web School")
   course.printDetails("Teacher")
@@ -10,20 +9,33 @@ fun main() {
   println("Casted Teacher:")
   castedCourse.printTeacherDetails()
 
-  //cast teacher to course
-  val castedTeacher = teacher as Course
-  castedTeacher.CourseAttendersCount = 20000
-  castedTeacher.CourseStatus = "Ongoing"
-  castedCourse.printDetails("Teacher")
+
+
 
   var student = Student("Arash Moradi" , "200#489" , "19.5" ,
                         "202.B","MaktabKhoneh","MaktabKhoneh.org Web School")
   student.printDetails("Student")
 
-  var book = Book("Android Studio" , "Programmers unity" , "Java Katlin FXML and Developing Android apps",
-    "Nobody","Android Development","Programming")
-  book.printDetails()
+  //cast student to school
+  val castedStudent = student as School
+  println("Casted School:")
+  castedStudent.printDetails("Casted ")
 
-  var classIntf = Class()
-  classIntf.printLocation()
+  val book = Book("Android Studio" , "Programmers unity" , "Java Katlin FXML and Developing Android apps",
+    "Nobody","Android Development","Programming")
+
+
+  val classInfo = Class()
+
+  //cast with condition
+  caster(book)
+  caster(classInfo)
+
+}
+
+fun caster(castingObject:Any){
+  when(castingObject){
+    is Book ->  castingObject.printDetails()
+    is Class ->  castingObject.printLocation()
+  }
 }
