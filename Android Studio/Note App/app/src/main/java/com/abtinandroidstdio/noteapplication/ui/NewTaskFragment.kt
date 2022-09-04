95d84fa03d93ad24b3dcccff5a654e266f18cd32
Abtinz <abtinzandi@gmail.com>
2022-09-04 17:59:20 +0430

    desining my note page

--- Android Studio/Note App/app/src/main/java/com/abtinandroidstdio/noteapplication/ui/NewTaskFragment.kt
+++ Android Studio/Note App/app/src/main/java/com/abtinandroidstdio/noteapplication/ui/NewTaskFragment.kt
@@ -1,18 +1,18 @@
 package com.abtinandroidstdio.noteapplication.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.abtinandroidstdio.noteapplication.R
-
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
-class HomeFragment : Fragment() {
+class NewTaskFragment : Fragment() {
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -28,7 +28,9 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_task, container, false)
     }
+
 
 }
