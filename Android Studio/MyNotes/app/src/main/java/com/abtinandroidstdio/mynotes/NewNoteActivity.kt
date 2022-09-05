2ae434a58bfcd432597d1cec03ea5cd1d3a7066f
Abtinz <abtinzandi@gmail.com>
2022-09-05 17:14:30 +0430

    lets make a new class for our room add sevice

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/NewNoteActivity.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/NewNoteActivity.kt
@@ -1,11 +1,15 @@
 package com.abtinandroidstdio.mynotes
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.abtinandroidstdio.mynotes.databinding.ActivityNewNoteBinding
+import com.abtinandroidstdio.mynotes.db.Note
+import com.abtinandroidstdio.mynotes.db.NoteDataBase
+
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding : ActivityNewNoteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +24,29 @@ class NewNoteActivity : AppCompatActivity() {
 
         if(newNoteTitle.isEmpty()){
             Toast.makeText(this,"please write your new note title!",Toast.LENGTH_SHORT).show()
+        }else{
+            val newNote = Note(newNoteTitle,newNoteText)
+            NoteDataBase(this).getNoteDB().addNote(newNote)
+            val intent = Intent(this , MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
-        val intent = Intent(this , MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
+
+    }
+
+    fun saveNewNote(note: Note){
+        class SaveNewNote: AsyncTask<Void,Void,Void>(){
+            override fun doInBackground(vararg p0: Void?): Void? {
+                NoteDataBase(this@NewNoteActivity).getNoteDB().addNote(note)
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+            }
+
+        }
+        SaveNewNote().execute()
     }
 
 
