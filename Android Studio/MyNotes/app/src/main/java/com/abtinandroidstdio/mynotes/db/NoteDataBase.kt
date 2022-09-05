22d40ad52cb8d84e44e5a0b46dd3ea65a18000a8
Abtinz <abtinzandi@gmail.com>
2022-09-05 16:46:06 +0430

    make invoke and builder

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/db/NoteDataBase.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/db/NoteDataBase.kt
@@ -1,16 +1,34 @@
 package com.abtinandroidstdio.mynotes.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [Note::class] ,
     version = 1
 )
-abstract class NoteDataBase {
+abstract class NoteDataBase :RoomDatabase(){
     abstract fun getNoteDB():NoteDB
 
     companion object{
+
         @Volatile private var instance : NoteDataBase? = null
+
+        private val LOCK = Any()
+
+        private fun dbBuilder(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            NoteDataBase::class.java,
+            "notesDB"
+        ).build()
+
+        operator fun invoke(context : Context) = instance ?: synchronized(LOCK){
+            instance ?: dbBuilder(context).also {
+                instance = it
+            }
+        }
 
     }
 }
