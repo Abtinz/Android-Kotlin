78d4de38e19280045cad4fd41aec7defda26c405
Abtinz <abtinzandi@gmail.com>
2022-09-05 14:19:45 +0430

    how to use Query in room library

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/db/NoteDB.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/db/NoteDB.kt
@@ -2,11 +2,17 @@ package com.abtinandroidstdio.mynotes.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface NoteDB {
 
-    
+
     @Insert
     fun addNote(note: Note)//add new note
+
+    @Query("SELECT * FROM note")
+    fun getAllNotes():List<Note>
+
+    
 }
