daa1892e78ffad877f5d53cb4c11940df9bda25a
Abtinz <abtinzandi@gmail.com>
2022-09-06 18:34:20 +0430

    how to set  fragment

--- Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/slideshow/SlideshowFragment.kt
+++ Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/slideshow/SlideshowFragment.kt
@@ -4,39 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.abtinandroidstdio.andoridquiz.databinding.FragmentSlideshowBinding
+import com.abtinandroidstdio.andoridquiz.R
 
 class SlideshowFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return inflater.inflate(R.layout.fragment_slideshow, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
