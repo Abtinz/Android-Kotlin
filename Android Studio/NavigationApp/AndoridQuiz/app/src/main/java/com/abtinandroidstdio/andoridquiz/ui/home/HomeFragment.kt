82d3fab7163c17677ef67a3deb398bec38a9e5de
Abtinz <abtinzandi@gmail.com>
2022-09-06 18:56:32 +0430

    set the main page

--- Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/home/HomeFragment.kt
+++ Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.abtinandroidstdio.andoridquiz.R
-
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
