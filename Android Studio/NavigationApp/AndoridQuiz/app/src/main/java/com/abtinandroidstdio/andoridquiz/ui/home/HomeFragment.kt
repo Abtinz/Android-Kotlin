1cb1d78f10b812e530099f74a5e0245f50c575ce
Abtinz <abtinzandi@gmail.com>
2022-09-07 10:18:29 +0430

    how to work with binding in fragments

--- Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/home/HomeFragment.kt
+++ Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/home/HomeFragment.kt
@@ -4,25 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.abtinandroidstdio.andoridquiz.R
 import com.abtinandroidstdio.andoridquiz.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
-    
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        val binding = FragmentHomeBinding.inflate(layoutInflater)
+        val binding = FragmentHomeBinding.inflate(inflater)
         binding.button.setOnClickListener{
-            Navigation.findNavController(it).navigate(R.id.action_nav_home_to_nav_gallery)
+            Navigation.findNavController(it).navigate(R.id.action_nav_home_to_quizQuestionsFragment)
         }
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        return binding.root
     }
 
 
