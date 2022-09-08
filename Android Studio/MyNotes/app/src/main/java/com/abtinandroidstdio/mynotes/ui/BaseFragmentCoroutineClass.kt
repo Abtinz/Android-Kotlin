7e1fc021ade78e653a0927a089a4264b47a50759
Abtinz <abtinzandi@gmail.com>
2022-09-08 22:58:01 +0430

    new class for all fragments --> CoroutineScope

---
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
@@ -0,0 +1,8 @@
+package com.abtinandroidstdio.mynotes.ui
+
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+
+abstract class BaseFragmentCoroutineClass:Fragment(),CoroutineScope {
+
+}
