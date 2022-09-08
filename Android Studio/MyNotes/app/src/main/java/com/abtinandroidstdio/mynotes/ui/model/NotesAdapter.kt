2ec5a5002debc67860c80010beb7e4b0717a5512
Abtinz <abtinzandi@gmail.com>
2022-09-09 00:03:21 +0430

    set our object in constructor

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/model/NotesAdapter.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/model/NotesAdapter.kt
@@ -1,9 +1,23 @@
 package com.abtinandroidstdio.mynotes.ui.model
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.abtinandroidstdio.mynotes.db.Note
 
-class NotesAdapter:RecyclerView.Adapter<>() {
+class NotesAdapter(val notesList :List<Note>):RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
 
     class ViewHolder(val view:View):RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
