0cc7e35ea2d61c5ad9b93b665908842bfe836a02
Abtinz <abtinzandi@gmail.com>
2022-09-08 15:35:50 +0430

    how to set argomans

--- Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/Game/QuizQuestions/QuizQuestionsFragment.kt
+++ Android Studio/NavigationApp/AndoridQuiz/app/src/main/java/com/abtinandroidstdio/andoridquiz/ui/Game/QuizQuestions/QuizQuestionsFragment.kt
@@ -16,12 +16,17 @@ class QuizQuestionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentQuizQusetionsBinding.inflate(inflater)
-
+        var choosedOption = "You have not chose any option "
         binding.submitButton.setOnClickListener{
             if(binding.roomCheckBox.isChecked){
                 Navigation.findNavController(it).navigate(QuizQuestionsFragmentDirections.actionQuizQuestionsFragmentToWiningGameFragment())
             }else{
-                Navigation.findNavController(it).navigate(QuizQuestionsFragmentDirections.actionQuizQuestionsFragmentToGameOverFragment2())
+                if(binding.retrofitCheckBox.isChecked){
+                    choosedOption = "Retrofit is wrong answer"
+                }else{
+                    choosedOption = "Navigation is wrong answer"
+                }
+                Navigation.findNavController(it).navigate(QuizQuestionsFragmentDirections.actionQuizQuestionsFragmentToGameOverFragment2(choosedOption))
             }
         }
 
