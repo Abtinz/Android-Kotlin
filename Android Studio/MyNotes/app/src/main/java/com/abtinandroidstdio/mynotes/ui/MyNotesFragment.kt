533a62a6e3620e4120c8b14fee8f02326be729c9
Abtinz <abtinzandi@gmail.com>
2022-09-08 18:35:46 +0430

    home layout

---
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/MyNotesFragment.kt
@@ -0,0 +1,25 @@
+package com.abtinandroidstdio.mynotes.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abtinandroidstdio.mynotes.R
+import com.abtinandroidstdio.mynotes.databinding.FragmentMyNotesBinding
+
+
+class MyNotesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMyNotesBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+
+}
