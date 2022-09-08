72386e31c5cb065a4d890b4dd04237b6ba68d2aa
Abtinz <abtinzandi@gmail.com>
2022-09-08 23:01:17 +0430

    Dispatchers will help us to find our threads in kotlin

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
@@ -2,10 +2,14 @@ package com.abtinandroidstdio.mynotes.ui
 
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
 abstract class BaseFragmentCoroutineClass:Fragment(),CoroutineScope {
 
     private lateinit var job : Job
-    
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers
 }
