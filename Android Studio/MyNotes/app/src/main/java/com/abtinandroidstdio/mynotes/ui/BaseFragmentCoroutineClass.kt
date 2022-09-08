74c10d9b3cfa22d04863887a00a525e804615ae6
Abtinz <abtinzandi@gmail.com>
2022-09-08 23:04:48 +0430

    when fragment is destroyed --> canceling the job

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
@@ -1,5 +1,6 @@
 package com.abtinandroidstdio.mynotes.ui
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,5 +12,17 @@ abstract class BaseFragmentCoroutineClass:Fragment(),CoroutineScope {
     private lateinit var job : Job
 
     override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers
+        get() = job + Dispatchers.Main
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    //when fragment is destroyed --> canceling the job
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
 }
