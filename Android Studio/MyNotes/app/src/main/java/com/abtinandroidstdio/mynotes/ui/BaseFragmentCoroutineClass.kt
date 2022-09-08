e6cf98ceb8c1a741af839122b871f8a2c978ff02
Abtinz <abtinzandi@gmail.com>
2022-09-08 22:59:38 +0430

    Job is a background task in android

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/BaseFragmentCoroutineClass.kt
@@ -2,7 +2,10 @@ package com.abtinandroidstdio.mynotes.ui
 
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 
 abstract class BaseFragmentCoroutineClass:Fragment(),CoroutineScope {
 
+    private lateinit var job : Job
+    
 }
