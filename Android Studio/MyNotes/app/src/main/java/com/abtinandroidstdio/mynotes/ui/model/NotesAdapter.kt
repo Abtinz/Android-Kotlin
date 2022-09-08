c73fc5524a94f8d795f298b218c5a2bceba78a6e
Abtinz <abtinzandi@gmail.com>
2022-09-09 00:00:34 +0430

    our adding model -->  class ViewHolder(val view:View):RecyclerView.ViewHolder(view)

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/model/NotesAdapter.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/model/NotesAdapter.kt
@@ -1,6 +1,9 @@
 package com.abtinandroidstdio.mynotes.ui.model
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class NotesAdapter:RecyclerView.Adapter<>() {
+
+    class ViewHolder(val view:View):RecyclerView.ViewHolder(view)
 }
