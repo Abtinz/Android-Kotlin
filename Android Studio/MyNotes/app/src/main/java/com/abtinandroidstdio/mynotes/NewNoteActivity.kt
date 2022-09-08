61e9f0a97c2c448901049047978755d46a4d851f
Abtinz <abtinzandi@gmail.com>
2022-09-08 19:22:47 +0430

    set important conditions to our fragment

--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/NewNoteActivity.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/NewNoteActivity.kt
@@ -18,23 +18,6 @@ class NewNoteActivity : AppCompatActivity() {
         val newNoteTitle = binding.title.text.toString().trim()
         val newNoteText = binding.noteText.text.toString().trim()
 
-        if(newNoteTitle.isEmpty()){
-            Toast.makeText(this,"please write your new note title!",Toast.LENGTH_SHORT).show()
-
-        }else{
-            Toast.makeText(this,newNoteTitle,Toast.LENGTH_SHORT).show()
-            val newNote = Note(newNoteTitle,newNoteText)
-            try {
-
-                NoteDataBase(this@NewNoteActivity).getNoteDB().addNote(newNote)
-                val intent = Intent(this , MainActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            }catch (exep : Exception){
-               exep.printStackTrace()
-            }
-
-        }
 
     }*/
 
--- Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/NewNoteFragment.kt
+++ Android Studio/MyNotes/app/src/main/java/com/abtinandroidstdio/mynotes/ui/NewNoteFragment.kt
@@ -5,18 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.abtinandroidstdio.mynotes.R
 import com.abtinandroidstdio.mynotes.databinding.FragmentNewNoteBinding
-
+import com.abtinandroidstdio.mynotes.db.Note
+import com.abtinandroidstdio.mynotes.db.NoteDataBase
 
 class NewNoteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         val binding = FragmentNewNoteBinding.inflate(inflater)
+
+        binding.saveNewNoteButton.setOnClickListener {
+            val newNoteTitle = binding.newNoteTitle.text.toString().trim()
+            val newNoteText = binding.newNoteText.text.toString().trim()
+
+            if(newNoteTitle.isEmpty()){
+                Toast.makeText(context,"please write your new note title!",Toast.LENGTH_SHORT).show()
+
+            }else{
+                Toast.makeText(context,newNoteTitle,Toast.LENGTH_SHORT).show()
+                val newNote = Note(newNoteTitle,newNoteText)
+                try {
+                    //NoteDataBase(context!!).getNoteDB().addNote(newNote)
+                    Navigation.findNavController(it).navigate(R.id.action_newNoteFragment_to_myNotesFragment)
+                }catch (exception : Exception){
+                    exception.printStackTrace()
+                }
+
+            }
+        }
 
         return binding.root
     }
