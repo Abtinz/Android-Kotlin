a02c2f8c6594fb8e0a9c7300a0a8207d566dc47c
abtinz <abtinzand@gmail.com>
2023-11-11 23:17:13 +0330

    FontStyle

--- Jetpack Compose/BasicCompose/app/src/main/java/com/example/basiccompose/text/TextViews.kt
+++ Jetpack Compose/BasicCompose/app/src/main/java/com/example/basiccompose/text/TextViews.kt
@@ -3,7 +3,9 @@ package com.example.basiccompose.text
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -33,6 +35,19 @@ fun TextFunction() {
                 "Hi Android",
                 fontSize = with(density){15.dp.toSp()},
                 lineHeight = 100.sp
+            )
+
+            Text(
+                "Hi Android",
+                fontSize = with(density){15.dp.toSp()},
+                color = Color.Red
+            )
+
+            Text(
+                "Hi Android",
+                fontSize = with(density){15.dp.toSp()},
+                color = Color.Red,
+                fontStyle = FontStyle.Italic
             )
         }
     }
