fef2379a0c46e663c0fc25f13a446aa1e691fa7a
abtinz <abtinzand@gmail.com>
2023-11-11 23:04:36 +0330

    new project for simple rehearsing

---
+++ CE-MP-2023/BusinessCard/app/src/main/java/com/example/businesscard/ui/screen/test/TestUi.kt
@@ -0,0 +1,10 @@
+package com.example.businesscard.ui.screen.test
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+
+}
---
+++ CE-MP-2023/BusinessCard/app/src/main/java/com/example/businesscard/utils/InformationStrings.kt
@@ -0,0 +1,21 @@
+package com.example.businesscard.utils
+
+object InformationStrings {
+    const val fullname = "Abtin Zandi"
+    const val location = "Iran/Tehran"
+    const val job = "Android Developer"
+    const val company = "Uritect"
+    const val university = "Amirkabir University of Technology"
+}
+
+object SkillsString {
+    const val android = "Android Development(Kotlin)"
+    const val recommenderSystem = "Recommender Systems"
+    const val nlp = "Natural Language Processing"
+    const val cv = "Computer Vision"
+}
+
+object ContactString{
+    const val email = "abtinzandi@gmail.com"
+    const val github = "github/Abtinz"
+}
