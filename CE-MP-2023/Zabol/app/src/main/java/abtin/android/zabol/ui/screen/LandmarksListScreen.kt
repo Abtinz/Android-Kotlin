93bfe3be8514e08d2fe0ec33af72b625dac1fad3
abtinz <abtinzand@gmail.com>
2023-11-12 11:30:25 +0330

    Laandmark List view

--- CE-MP-2023/Zabol/app/src/main/java/abtin/android/zabol/ui/screen/LandmarksListScreen.kt
+++ CE-MP-2023/Zabol/app/src/main/java/abtin/android/zabol/ui/screen/LandmarksListScreen.kt
@@ -1,8 +1,22 @@
 package abtin.android.zabol.ui.screen
 
+import abtin.android.zabol.model.Landmarks
+import abtin.android.zabol.ui.theme.mainBackGround
+import androidx.compose.foundation.background
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun LandmarksListScreen() {
-    
+fun LandmarksListScreen(landmarks :List<Landmarks>) {
+    LazyColumn(
+        Modifier.background(color = mainBackGround)
+    ){
+        items( landmarks){ it ->
+            LandmarksCardView(
+                landmarkInfo = it
+            )
+        }
+    }
 }
