bc112bfe01beb3ed1e9b9a9f4bce7899f235ac42
abtinz <abtinzand@gmail.com>
2023-11-12 10:44:46 +0330

    font types!

--- CE-MP-2023/Zabol/app/src/main/java/abtin/android/zabol/ui/theme/Type.kt
+++ CE-MP-2023/Zabol/app/src/main/java/abtin/android/zabol/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package abtin.android.zabol.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.businesscard.R
+
+val thinoothinFont = FontFamily(
+    Font(R.font.thinoothin)
+)
+
+val thinoobold = FontFamily(
+    Font(R.font.thinoobold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +24,18 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
+    titleMedium = TextStyle(
+    fontFamily = thinoobold,
+    fontWeight = FontWeight.Bold
+    ),
+
+    displaySmall = TextStyle(
+        fontFamily = thinoothinFont,
+        fontWeight = FontWeight.SemiBold
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
