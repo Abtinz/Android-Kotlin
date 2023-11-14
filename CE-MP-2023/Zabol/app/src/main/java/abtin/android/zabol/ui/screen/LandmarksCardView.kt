7d20260624a54bbb2e14c864bf6e02264564ff9b
abtinz <abtinzand@gmail.com>
2023-11-14 18:20:36 +0330

    expandable text debugging

--- CE-MP-2023/Zabol/app/src/main/java/abtin/android/zabol/ui/screen/LandmarksCardView.kt
+++ CE-MP-2023/Zabol/app/src/main/java/abtin/android/zabol/ui/screen/LandmarksCardView.kt
@@ -1,11 +1,17 @@
 package abtin.android.zabol.ui.screen
 
+import abtin.android.zabol.ExpandableText
 import abtin.android.zabol.model.Landmarks
 import abtin.android.zabol.ui.theme.informationText
 import abtin.android.zabol.ui.theme.primary
 import abtin.android.zabol.ui.theme.tertiary
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,12 +25,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -35,8 +47,14 @@ import com.example.businesscard.R
 fun LandmarksCardView(landmarkInfo:Landmarks) {
     Card(
         modifier = Modifier
-                .padding(bottom = 5.dp,top = 5.dp , end = 10.dp , start = 10.dp)
-                .background(color = tertiary, shape = RoundedCornerShape(8.dp) ),
+            .padding(bottom = 5.dp, top = 5.dp, end = 10.dp, start = 10.dp)
+            .background(color = tertiary, shape = RoundedCornerShape(8.dp))
+            .animateContentSize(
+                animationSpec = tween(
+                    easing = LinearOutSlowInEasing,
+                    durationMillis = 200
+                )
+            ),
         shape = RoundedCornerShape(8.dp)
     ) {
         Column(
@@ -85,16 +103,25 @@ fun LandmarksCardView(landmarkInfo:Landmarks) {
                 )
             }
 
-            Text(
-                text = landmarkInfo.description,
-                textAlign = TextAlign.Justify,
-                fontFamily = FontFamily.SansSerif,
+            var expand by remember {
+                mutableStateOf(false)
+            }
+
+            ExpandableText(
+                originalText = landmarkInfo.description,
+                expandAction = "See More",
+                expand = expand,
+                expandActionColor = Color.DarkGray,
                 fontSize = with(density){16.dp.toSp()},
+                fontStyle = FontStyle.Normal,
                 color = informationText,
+                fontFamily = FontFamily.SansSerif,
+                limitedMaxLines = 2,
+                animationSpec = spring(),
                 modifier = Modifier
+                    .clickable { expand = !expand }
                     .padding(10.dp)
             )
-
         }
     }
 }
