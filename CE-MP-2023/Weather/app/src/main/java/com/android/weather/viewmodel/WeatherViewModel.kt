8bf631d3a85bc5b4532a8e4fb7ec0df4309eaa42
Abtinz <abtinzandi@gmail.com>
2023-12-30 02:13:21 +0330

    view model is ready

--- CE-MP-2023/Weather/app/src/main/java/com/android/weather/viewmodel/WeatherViewModel.kt
+++ CE-MP-2023/Weather/app/src/main/java/com/android/weather/viewmodel/WeatherViewModel.kt
@@ -1,7 +1,41 @@
 package com.android.weather.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.weather.network.api.RetrofitClient
+import com.android.weather.network.response.WeatherResponse
+import com.android.weather.network.url.BasicApiUrls.API_KEY
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class WeatherViewModel():ViewModel() {
 
+    private val _weatherResponse  = MutableStateFlow<WeatherResponse?>(null)
+    val weatherResponse: StateFlow<WeatherResponse?> get() = _weatherResponse
+
+    private val _apiStatus  = MutableStateFlow("")
+    val apiStatus:StateFlow<String>  get() = _apiStatus
+
+    fun weatherApi(lat:Double,lon:Double){
+        viewModelScope.launch {
+            try{
+
+                _weatherResponse.value = RetrofitClient.apiService.weatherApi(
+                    longitude =  lon,
+                    latitude = lat,
+                    apiKey = API_KEY
+                )
+
+                _apiStatus.value = "200"
+            }catch (exception:Exception){
+                exception.printStackTrace()
+                if (exception.message.toString().contains("50")) {
+                    _apiStatus.value = "server time out"
+                }else{
+                    _apiStatus.value = "some things went wrong"
+                }
+            }
+        }
+    }
 }
