2a62a40c73443f84022aea5f206c7b88c425b520
Abtinz <abtinzandi@gmail.com>
2023-12-29 23:49:49 +0330

    new urls

--- CE-MP-2023/Weather/app/src/main/java/com/android/weather/network/url/ApiUtils.kt
+++ CE-MP-2023/Weather/app/src/main/java/com/android/weather/network/url/ApiUtils.kt
@@ -2,6 +2,7 @@ package com.android.weather.network.url
 
 object BasicApiUrls {
     const val Weather_BASIC_URL = "https://api.openweathermap.org/data/2.5/weather"
+    const val BASIC_URL = "https://api.openweathermap.org/data/2.5/"
     const val Air_pollution_BASIC_URL = "https://aqms.doe.ir/Home/LoadAQIMap?id=1"
     const val API_KEY = "ead59696424fdb3a37727b89a78f1e2b"
 }
