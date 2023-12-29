d2a2467fcdb1cc9ef9669f03988a206d7941a899
Abtinz <abtinzandi@gmail.com>
2023-12-30 01:44:40 +0330

    screen route updating

--- CE-MP-2023/Weather/app/src/main/java/com/android/weather/ui/model/screens/WeatherScreens.kt
+++ CE-MP-2023/Weather/app/src/main/java/com/android/weather/ui/model/screens/WeatherScreens.kt
@@ -9,32 +9,14 @@ sealed class WeatherScreens(val route:String) {
 
     object MainMenu : WeatherScreens(route = "home")
 
-    object Location : WeatherScreens(route = "loation")
+    object Location : WeatherScreens(route = "location")
     object Map : WeatherScreens(route = "map")
     object CityList : WeatherScreens(route = "city_list")
     object Search : WeatherScreens(route = "search")
 
-    object WeatherScreen: WeatherScreens(route = "weather/page/{location}"){
-        fun passInfo(landmarks: LandmarksDC):String{
-            val moshi = Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-            val jsonAdapter = moshi.adapter(LandmarksDC::class.java).lenient()
-            val landmarkJson = URLEncoder.encode(jsonAdapter.toJson(landmarks), "UTF-8")
-
-            return "landmarks/page/${landmarkJson}"
-        }
-    }
-
-    object AirPollutionScreen: WeatherScreens(route = "airpollution/page/{location}"){
-        fun passInfo(landmarks: LandmarksDC):String{
-            val moshi = Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-            val jsonAdapter = moshi.adapter(LandmarksDC::class.java).lenient()
-            val landmarkJson = URLEncoder.encode(jsonAdapter.toJson(landmarks), "UTF-8")
-
-            return "landmarks/page/${landmarkJson}"
+    object WeatherScreen: WeatherScreens(route = "weather/page/{lat}/{lon}"){
+        fun passInfo(lat:Double, lan:Double):String{
+            return "weather/page/${lat.toString()}/${lan.toString()}"
         }
     }
 
--- CE-MP-2023/Weather/app/src/main/java/com/android/weather/ui/screen/weather/WeatherScreen.kt
+++ CE-MP-2023/Weather/app/src/main/java/com/android/weather/ui/screen/weather/WeatherScreen.kt
@@ -1,2 +1,9 @@
 package com.android.weather.ui.screen.weather
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherScreen(navController: NavController , lat :Double , lon:Double) {
+    println(lat )
+}
