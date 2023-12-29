5613532db094f228b8453033ac201d0155cde8a6
Abtinz <abtinzandi@gmail.com>
2023-12-30 01:44:10 +0330

    navigation graph updating ...

--- CE-MP-2023/Weather/app/src/main/java/com/android/weather/ui/model/navigation/NavGraph.kt
+++ CE-MP-2023/Weather/app/src/main/java/com/android/weather/ui/model/navigation/NavGraph.kt
@@ -3,12 +3,15 @@ package com.android.weather.ui.model.navigation
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.android.weather.ui.model.screens.WeatherScreens
 import com.android.weather.ui.screen.home.MainMenuScreen
 import com.android.weather.ui.screen.location.LocationScreen
 import com.android.weather.ui.screen.search.SearchScreen
+import com.android.weather.ui.screen.weather.WeatherScreen
 
 @ExperimentalMaterialApi
 @Composable
@@ -36,6 +39,29 @@ fun NavigationGraph (navHostController: NavHostController){
 
         composable(route = WeatherScreens.CityList.route) {
             MainMenuScreen(navController = navHostController)
+        }
+
+        composable(route = WeatherScreens.WeatherScreen.route) {
+            MainMenuScreen(navController = navHostController)
+        }
+
+        composable(route = WeatherScreens.WeatherScreen.route,
+            arguments = listOf(
+                navArgument("lat") {
+                    type = NavType.StringType
+                },
+                navArgument("lon") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val lat = it.arguments!!.getString("lat")!!.toDouble()
+            val lon = it.arguments!!.getString("lon")!!.toDouble()
+            WeatherScreen(
+                lat = lat,
+                lon = lon,
+                navController = navHostController
+            )
         }
 
     }
