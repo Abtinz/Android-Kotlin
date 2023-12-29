befdabf648a5766ed24258d9cfdc042d7b95f913
Abtinz <abtinzandi@gmail.com>
2023-12-29 23:49:33 +0330

    api service is implemented

--- CE-MP-2023/Weather/app/src/main/java/com/android/weather/network/api/ApiService.kt
+++ CE-MP-2023/Weather/app/src/main/java/com/android/weather/network/api/ApiService.kt
@@ -1,16 +1,25 @@
 package com.android.weather.network.api
 
 
-import com.android.weather.network.response.PollutionResponse
+import com.android.weather.network.response.AirPollutionResponse
 import com.android.weather.network.response.WeatherResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("")
-    suspend fun weatherPage(): WeatherResponse
+    @GET("forecast")
+    suspend fun weatherApi(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") apiKey: String
+    ): WeatherResponse
 
-    @GET("")
-    suspend fun pollutionInfo(): List<PollutionResponse>
+    @GET("air_pollution")
+    suspend fun pollutionInfo(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") apiKey: String
+    ): AirPollutionResponse
 
 }
