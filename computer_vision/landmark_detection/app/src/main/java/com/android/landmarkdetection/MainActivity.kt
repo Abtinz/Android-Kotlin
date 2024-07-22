9f37068715783627d94013e5b4b7c7181dddd6af
Abtinz <abtinzandi@gmail.com>
2024-07-22 17:06:36 +0330

    hasCameraPermission CHECK

--- computer_vision/landmark_detection/app/src/main/java/com/android/landmarkdetection/MainActivity.kt
+++ computer_vision/landmark_detection/app/src/main/java/com/android/landmarkdetection/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.android.landmarkdetection
 
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.landmarkdetection.ui.theme.LandmarkDetectionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), 0
+            )
+        }
         setContent {
             LandmarkDetectionTheme {
 
             }
         }
     }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
 }
