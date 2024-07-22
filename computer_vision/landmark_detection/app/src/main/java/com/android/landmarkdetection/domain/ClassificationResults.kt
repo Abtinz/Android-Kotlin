6d424968b1172b05c7ee1dac70f4d82b5bb90bba
Abtinz <abtinzandi@gmail.com>
2024-07-22 15:42:06 +0330

    ClassificationResults

---
+++ computer_vision/landmark_detection/app/src/main/java/com/android/landmarkdetection/domain/ClassificationResults.kt
@@ -0,0 +1,12 @@
+package com.android.landmarkdetection.domain
+
+/**
+ * Data class to represent the results of a classification operation.
+ *
+ * @property label The label or category assigned by the classification algorithm.
+ * @property score The confidence score or probability associated with the label.
+ */
+data class ClassificationResults(
+    val label: String,
+    val score: String
+)
